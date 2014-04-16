6b38059bc6f25e974f613b936e98610e50752c2f
robin92pl <robin92pl@gmail.com>
2014-04-16 10:22:56 +0200

    Implemented signing in and out.

---
+++ js/util/loginmanager.js
@@ -0,0 +1,75 @@
+
+define([
+    'jquery',
+], function($) {
+    var Storage = (function() {
+        return {
+            getInstance: function () {
+                return sessionStorage;  
+            },
+        };
+    })();
+    
+    var loginManager = (function(settings) {        
+        var defaults = {
+            loginUrl: '/auth/signin',
+        };
+        
+        var mLoginUrl = defaults.loginUrl;
+        
+        var storeData = function(data) {
+            Storage.getInstance().flavority = JSON.stringify(data);
+        };
+        
+        var restoreData = function() {
+            var data = Storage.getInstance().flavority;
+            if (data != null && data != '') {
+                return JSON.parse(data);
+            }
+            
+            return null;
+        };
+        
+        var removeData = function() {
+            Storage.getInstance().removeItem('flavority');
+        };
+        
+        var login = function(username, password, params) {
+            $.ajax(mLoginUrl, {
+                type: 'POST',
+                cache: false,
+                data: {
+                    email: username,
+                    password: password,
+                },
+                error: params.error,
+                success: function(data, status, jx) {                    
+                    storeData({
+                        token: data.token,
+                    });
+                    
+                    if (params.success != null) params.success(data, status, jx);
+                },
+            });
+        };
+        
+        var logout = function() {
+            removeData();
+        };
+        
+        var isLogged = function() {
+            var data = restoreData();           
+            if (data == null || data.token == null) return false;           
+            return true;
+        };
+                        
+        return {
+            login: login,
+            logout: logout,
+            isLogged: isLogged,
+        };
+    })();
+
+    return loginManager;
+});
+
--- js/views/panelTop.js
+++ js/views/panelTop.js
@@ -3,18 +3,69 @@ define([
     'jquery',
     'underscore',
     'backbone',
-
+    
+    'util/loginmanager',
+    
     'text!templates/logged.html',
     'text!templates/not_logged.html',
-], function($, _, Backbone, loggedTemplate, notLoggedTemplate) {
-	
+], function($, _, Backbone, loginManager, loggedTemplate, notLoggedTemplate) {	
 	var PanelTopView = Backbone.View.extend({
 		el: $("#panelTop"),
+		formVisible: false,
+		userMenuVisible: false,
+		
+		events: {
+			'click #log_in a': 'triggerLoginForm',
+			'submit #login-form form': 'onLoginFormSubmit',
+		    
+			'click #actions_button1': 'onTriggerUserMenu',
+			'click #user_actions a[name=logout]': 'onLogout',
+		},
+		
+		triggerLoginForm: function() {
+			if (!this.formVisible) $("#login-form").show();
+			else $("#login-form").hide();
+			this.formVisible = !this.formVisible;
+		},
+		
+		onLoginFormSubmit: function() {
+			loginManager.login(
+				$('#login-form input[name=email]').val(),
+				$('#login-form input[name=password]').val(),
+				{
+					error: function(jrxhr, status, exception) {
+						alert(status + " " + exception);
+		            },
+					success: function(data, status, jrxhr) {
+						alert(status);		            
+						location.reload();
+					},
+				}
+			);
+		    
+			return false;   // no need to trigger default from behaviour
+		},
+		
+		onTriggerUserMenu: function(event) {
+			if (this.userMenuVisible) $('#user_actions').hide();
+			else $('#user_actions').show();
+			this.userMenuVisible = !this.userMenuVisible;
+		},
+		
+		onLogout: function() {
+			loginManager.logout();
+			location.reload();
+		},
+		
 		render: function() {
-			var compiledLoggedTemplate = _.template(loggedTemplate, {});
-			this.$el.html(compiledLoggedTemplate); 
+			var template = notLoggedTemplate;
+			if (loginManager.isLogged()) template = loggedTemplate;
+			
+			var compiledLoggedTemplate = _.template(template);
+			this.$el.html(compiledLoggedTemplate);
 		},
 	});
 	
 	return PanelTopView;
 });
+
