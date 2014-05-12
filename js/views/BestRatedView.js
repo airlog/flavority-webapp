89e65aeae5f103cb98a83dcfd262dffe6d374675
robin92pl <robin92pl@gmail.com>
2014-05-12 17:33:54 +0200

    Last added and best rated divs on homepage now use result template.

--- js/views/BestRatedView.js
+++ js/views/BestRatedView.js
@@ -8,8 +8,8 @@ define([
 
     'views/Spinner',
 
-    'text!templates/best_rated_recipes.html',
-], function ($, _, Backbone, RecipeCollection, Spinner, bestRatedRecipesTemplate) {
+    'text!templates/results.html',
+], function ($, _, Backbone, RecipeCollection, Spinner, resultsTemplate) {
     var elementString = ".recipe-best-rated ";
     var LastAddedView = Backbone.View.extend({
         el: elementString + '.data',
@@ -32,18 +32,22 @@ define([
                 },
 
                 success: function (collection, response, options) {
-                    var compiledTemplate = _.template(bestRatedRecipesTemplate, {
-                        random: function (min, max) {
-                            return Math.floor((Math.random() * max) + min);  
-                        },
-                        default_images: [
-                            // paths to default images (when no image for recipe)
-                            // TODO: use our images
-                            'http://images6.fanpop.com/image/photos/32900000/Sisters-applejack-my-little-pony-friendship-is-magic-32995815-300-300.jpg',
-                            'http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=37434267',
-                            'http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=42240617',
-                        ],
+                    var compiledTemplate = _.template(resultsTemplate, {
                         recipes: collection.models,
+                        getDefaultImage: function () {
+                            var images = [
+		                        // paths to default images (when no image for recipe)
+		                        // TODO: use our images
+		                        'http://images6.fanpop.com/image/photos/32900000/Sisters-applejack-my-little-pony-friendship-is-magic-32995815-300-300.jpg',
+		                        'http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=37434267',
+		                        'http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=42240617',
+                            ];
+                            var randint = function (min, max) {
+                                return Math.floor((Math.random() * max) + min);
+                            };
+
+                            return images[randint(0, images.length)];
+                        },
                     });
 
                     // stop the spinner
--- js/views/LastAddedView.js
+++ js/views/LastAddedView.js
@@ -8,8 +8,8 @@ define([
 
     'views/Spinner',
 
-    'text!templates/last_added_recipes.html',
-], function ($, _, Backbone, RecipeCollection, Spinner, lastAddedRecipesTemplate) {    
+    'text!templates/results.html',
+], function ($, _, Backbone, RecipeCollection, Spinner, resultsTemplate) {
     var elementString = ".recipe-last-added ";
     var LastAddedView = Backbone.View.extend({
         el: elementString + '.data',
@@ -32,18 +32,22 @@ define([
                 },
 
                 success: function (collection, response, options) {
-                    var compiledTemplate = _.template(lastAddedRecipesTemplate, {
-                        random: function (min, max) {
-                            return Math.floor((Math.random() * max) + min);
-                        },
+                    var compiledTemplate = _.template(resultsTemplate, {
                         recipes: collection.models,
-                        default_images: [
-                            // paths to default images (when no image for recipe)
-                            // TODO: use our images
-                            'http://images6.fanpop.com/image/photos/32900000/Sisters-applejack-my-little-pony-friendship-is-magic-32995815-300-300.jpg',
-                            'http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=37434267',
-                            'http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=42240617',
-                        ],
+                        getDefaultImage: function () {
+                            var images = [
+		                        // paths to default images (when no image for recipe)
+		                        // TODO: use our images
+		                        'http://images6.fanpop.com/image/photos/32900000/Sisters-applejack-my-little-pony-friendship-is-magic-32995815-300-300.jpg',
+		                        'http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=37434267',
+		                        'http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=42240617',
+                            ];
+                            var randint = function (min, max) {
+                                return Math.floor((Math.random() * max) + min);
+                            };
+
+                            return images[randint(0, images.length)];
+                        },
                     });
 
                     // stop the spinner
