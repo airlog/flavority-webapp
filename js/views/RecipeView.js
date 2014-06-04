c881ce331616010767fcb542a24cfe8a7db4c1cb
Joanna <joanna.cislo@gmail.com>
2014-06-04 23:24:01 +0200

    Added displaying recipes' images in all places. Closes #32.

--- js/views/RecipeView.js
+++ js/views/RecipeView.js
@@ -45,6 +45,25 @@ define([
                         recipe: recipe,
                         ingredients1: ingredients1,
                         ingredients2: ingredients2,
+                        getDefaultImage: function () {
+                            var images = [
+                                // paths to default images (when no image for recipe)
+                                // TODO: use our images
+                                'http://images6.fanpop.com/image/photos/32900000/Sisters-applejack-my-little-pony-friendship-is-magic-32995815-300-300.jpg',
+                                'http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=37434267',
+                                'http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=42240617',
+                            ];
+                            var randint = function (min, max) {
+                                return Math.floor((Math.random() * max) + min);
+                            };
+
+                            return images[randint(0, images.length)];
+                        },
+
+                        getImageUrl: function (imgId) {
+                            return window.app.restapiAddr + "/photos/" + imgId;
+                        },
+
                     });
                     this.$('#recipe_details').html(compiledRecipeTemplate);   
                     
